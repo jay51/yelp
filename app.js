885f24b901625e43ee1fde38edacf8855c78ff74
jay <nenoj51@gmail.com>
2018-03-26 03:18:55 +0000

    feat(onlye in app.js): connect to a database
    created a database to store names, images and descriptions of camps  and connected it to the app

--- app.js
+++ app.js
@@ -12,29 +12,31 @@ app.use(bodyParser.urlencoded({extended:true}));
 // DB schema
 const campgroundsSchema = mongoose.Schema({
 	name:String,
-	image:String
+	image:String,
+	description:String
 });
 
 // DB model 
 const Campground = mongoose.model("campgrounds", campgroundsSchema);
 
-// first campground
+// // first campground
 // Campground.create({
 // 	name:"big hells",
-// 	image:"https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197__340.jpg"
+// 	image:"https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197__340.jpg",
+// 	description:"nice view and water. No bath rooms but beatfull nature"
 	
 // }, (err, camp)=> console.log(err ? err : camp ));
 
 
 app.get("/", function(req, res){
-	res.render("index");
+	res.send("the main route '/'");
 });
 
 app.get("/campgrounds", function(req, res){
 	// find campgrounds from DB
 	Campground.find({}, (err, campgrounds)=>{
 		if(err) return err ;
-		res.render("campgrounds", {campgrounds});
+		res.render("index", {campgrounds});
 	});
 });
 
@@ -42,6 +44,7 @@ app.post("/campgrounds", function(req, res){
 	//get data from form and add to campgrounds arrey
 	let name = req.body.name;
 	let image = req.body.image;
+	let desc = req.body.descreption;
 	// add campground to DB
 	Campground.create({
 		name:name,
@@ -49,12 +52,22 @@ app.post("/campgrounds", function(req, res){
 	}, (err, camp)=>console.log(err ? err : camp));
 	
 	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	res.redirect("/index");
 });
 
 
 app.get("/campgrounds/new", function(req, res){
 	res.render("new");
+});
+
+
+app.get("/campgrounds/:id", function(req, res){
+	let campgroundId = req.params.id; 
+	Campground.findById(campgroundId, (err, foundCampground) =>{
+		if (err) return err;
+		console.log(foundCampground);
+		res.render("show", { foundCampground });
+	});
 });
 
 
