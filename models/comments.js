45c12fe4fe4b738d9376f4d298d88f3f0c87ee6d
jay51 <nenoj51@gmail.com>
2018-09-10 03:53:49 +0000

    modified:   models/comments.js fixed the comment modle was messing author obj 	modified:   routes/comments.js save the current user to the comment modle as the comment owner and added that only onwer of certine comment is allowed to edit comment 	modified:   views/auth/login.ejs

--- models/comments.js
+++ models/comments.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const commentsSchema = mongoose.Schema({
-    author: String,
-    text: String
+    text: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
 });
 // if you need to import this, import it as Comment, so you don't lose track of what's happening.
 module.exports = mongoose.model("Comment", commentsSchema);
--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const express = require("express"),
     mid = require("../middleware");
 
 const router = express.Router({ mergeParams: true }); // mergeParams wiil enable us to accesse the params inside our routes.
-
+// GET /new form
 router.get("/new", mid.isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) return err;
@@ -12,19 +12,48 @@ router.get("/new", mid.isLoggedIn, function(req, res) {
     });
 });
 
-
+// POST /  create new comment
 router.post("/", mid.isLoggedIn, function(req, res) {
     // find campground
     Campground.findById(req.params.id, (err, campground) => {
         if (err) return err; // need to handle err better
         // add comment to DB
-        Comment.create(req.body.comments, (err, comment) => {
+        Comment.create(req.body.comment, (err, comment) => {
             if (err) return err;
+            console.log(req.user);
+            // save the current user info
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            comment.save();
+
             campground.comments.push(comment);
             campground.save();
             res.redirect(`/campgrounds/${req.params.id}`);
         });
     });
+});
+
+
+router.get("/:comment_id/edit", function(req, res) {
+
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) return res.redirect("/campgrounds");
+        else {
+            return res.render("comments/edit", { campgroundId: req.params.id, comment: foundComment });
+        }
+    });
+});
+
+
+// POST /comment_id edit comments
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, req.body.comment,
+        function(err, comment) {
+            if (err) return res.redirect("back");
+            else {
+                return res.redirect(`/campgrounds/${req.params.id}`);
+            }
+        });
 });
 
 
